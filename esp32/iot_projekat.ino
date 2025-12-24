b6386f5247fd0ac158389fb12e5c9d7f9d7ff2e9
bpxtrik <patrik.barsi04@gmail.com>
2025-12-24 01:04:12 +0100

    Track esp32 code

---
+++ esp32/iot_projekat.ino
@@ -0,0 +1,37 @@
+const int sensorPin = 27;
+
+const int ledPins[] = {18, 19, 21, 22};
+const int ledCount = 4;
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(sensorPin, INPUT);
+
+  for (int i = 0; i < ledCount; i++) {
+    pinMode(ledPins[i], OUTPUT);
+    digitalWrite(ledPins[i], LOW);
+  }
+}
+
+void loop() {
+  int motion = digitalRead(sensorPin);
+
+  if (motion == HIGH) {
+    Serial.println("Motion detected");
+    activateLeds();
+
+    // prevent retrigger spam
+    delay(2000);
+  }
+}
+
+void activateLeds() {
+  for (int j = 0; j < 5; j++) {        // fixed comma → semicolon
+    for (int i = 0; i < ledCount; i++) {
+      digitalWrite(ledPins[i], HIGH);
+      delay(100);
+      digitalWrite(ledPins[i], LOW);
+    }
+  }
+}
