d29245c2d6238e5051657e9bf4e3cc45801b3265
Patrik Barsi <patrik.barsi04@gmail.com>
2025-12-25 15:06:20 +0100

    Add port to render

--- cmd/main.go
+++ cmd/main.go
@@ -5,20 +5,25 @@ import (
 	"home-system/internal/db"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
-
 func main() {
 	_ = godotenv.Load()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8090"
+	}
 
 	pool, err := db.New()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer pool.Close()
-	
+
 	h := &api.Handler{DB: pool}
 
 	mux := api.RegisterRoutes(h)
